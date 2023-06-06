243ca35159d6fa89b318f9106e2568c2b86018ab
Helen85236 <79532870251@yandex.ru>
2023-06-06 22:32:24 +0300

    changes

---
+++ src/vacancy_class.py
@@ -0,0 +1,17 @@
+"""Реализация класса для работы с вакансиями"""
+
+
+class Vacancy:
+    title = ''
+
+    def __init__(self, title, salary, description, url):
+        self.title = title
+        self.salary = salary
+        self.description = description
+        self.url = url
+
+    def __str__(self):
+        return f"title: {self.title},\n" \
+               f"salary:{self.salary},\n" \
+               f"description: {self.description},\n" \
+               f"url: {self.url}"
